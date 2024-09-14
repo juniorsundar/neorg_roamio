5b38d0e1dd0620ee7a8ff4d6c829f144a927af41
Junior Sundar <juniorsundar@gmail.com>
2024-09-15 00:50:11 +0400

    feat(watcher): Created a directory watcher

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/juniorsundar/neorg-roamio
 
 go 1.22.3
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 // Creating custom coloured loggers
 var (
-	errorLogger *log.Logger
-	warnLogger  *log.Logger
-	infoLogger  *log.Logger
+	logErr  *log.Logger
+	logWarn *log.Logger
+	logInfo *log.Logger
 )
 
 // Create prefixes to log messages with ANSI codes. I chose to do this over 3rd
@@ -34,9 +39,78 @@ func initLoggers(noColorCoding bool) {
 		infoPrefix = fmt.Sprintf("%sINFO: %s", green, reset)
 	}
 
-	errorLogger = log.New(os.Stderr, errorPrefix, log.LstdFlags|log.Lshortfile)
-	warnLogger = log.New(os.Stdout, warnPrefix, log.LstdFlags)
-	infoLogger = log.New(os.Stdout, infoPrefix, log.LstdFlags)
+	logErr = log.New(os.Stderr, errorPrefix, log.LstdFlags|log.Lshortfile)
+	logWarn = log.New(os.Stdout, warnPrefix, log.LstdFlags)
+	logInfo = log.New(os.Stdout, infoPrefix, log.LstdFlags)
+}
+
+// Initialise watcher for subdirectories recursively.
+//
+// @param watcher: Main `watcher` from main loop.
+//
+// @param dir: Root directory as source of recursive search.
+func addWatchDirRecursively(watcher *fsnotify.Watcher, dir string) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			if info.Name()[0] == '.' {
+				logWarn.Println("Skipping hidden directory:", path)
+				return filepath.SkipDir
+			}
+
+			err = watcher.Add(path)
+			if err != nil {
+				return err
+			}
+			logWarn.Printf("Watching: %s", path)
+		}
+		return nil
+	})
+}
+
+func dirWatcher(watcher *fsnotify.Watcher, verbose bool) {
+	for {
+		// Select is like Switch in that it observes what comes out of the
+		// channels. In this case, the two channels of interest are
+		// watcher.Events/Write. If any one of them gets a message then their
+		// respective case is triggered.
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			file, err := os.Stat(event.Name)
+
+			if verbose {
+				logInfo.Println("Event:", event.Name)
+			}
+
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				if err == nil && file.IsDir() {
+					err = addWatchDirRecursively(watcher, event.Name)
+					if err != nil {
+						logErr.Fatalln(err)
+					}
+				}
+			}
+
+			if event.Op&fsnotify.Remove == fsnotify.Remove {
+				if os.IsNotExist(err) {
+					watcher.Remove(event.Name)
+					logWarn.Printf("Removed %s from watcher.", event.Name)
+				}
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			logErr.Println(err)
+		}
+	}
 }
 
 func main() {
@@ -54,16 +128,31 @@ func main() {
 	if *verbosePtr {
 		flagString := fmt.Sprintf("\n\tDirectory: %s\n\tPort: %s\n\tVerbose: %t\n\tANSI Colors: %t\n",
 			*dirPtr, *portPtr, *verbosePtr, *noColorPtr)
-		infoLogger.Println(flagString)
+		logInfo.Println(flagString)
 	}
 
 	info, err := os.Stat(*dirPtr)
 	if os.IsNotExist(err) {
-		errorLogger.Fatalf("Directory %s does not exist.", *dirPtr)
+		logErr.Fatalf("Directory %s does not exist.", *dirPtr)
 	} else if os.IsPermission(err) {
-		errorLogger.Fatalf("Directory %s does not have edit permission.", *dirPtr)
+		logErr.Fatalf("Directory %s does not have edit permission.", *dirPtr)
 	}
 	if !info.IsDir() {
-		errorLogger.Fatalf("%s is not a directory", *dirPtr)
+		logErr.Fatalf("%s is not a directory", *dirPtr)
 	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		logErr.Fatalln("Something went wrong in creating watcher.")
+	}
+	defer watcher.Close()
+
+	err = addWatchDirRecursively(watcher, *dirPtr)
+	if err != nil {
+		logErr.Fatalln(err)
+	}
+	logWarn.Printf("Watching: %s", *dirPtr)
+
+	go dirWatcher(watcher, *verbosePtr)
+	<-make(chan struct{})
 }
